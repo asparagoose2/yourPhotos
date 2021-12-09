0b6c577da29e7566beb4bcbbff7b55bfa89aef04
Ofir <ofir.duchovne@gmail.com>
2021-12-09 23:25:59 +0200

    generating QR from csv file

--- image_proccessing/scan_folder.py
+++ image_proccessing/scan_folder.py
@@ -6,11 +6,14 @@ import zbarlight
 import qrcode
 import os
 from pathlib import Path
+import pandas as pd
+import uuid
+import datetime
 
 NULL_CODE = 'NULL'
+QR_CODE_URL = "localhost:5000/gallery/{}"
 
 owners = None
-event_global = "global"
 galleries = {}
 
 def scan_folder(folder):
@@ -32,11 +35,11 @@ def scan_image(img_path):
     codes = zbarlight.scan_codes('qrcode', img)
     if codes:
         if codes[0].decode('utf-8') == NULL_CODE:
-            print('NULL code')
+            print('[#] Found QR Code: {}'.format(NULL_CODE))
             owners = None
         else:
-            owners = [x.decode('utf-8').split('/')[-1] for x in codes]
             print('[+] Found QR Code: {}'.format(owners))
+            owners = [x.decode('utf-8').split('/')[-1] for x in codes]
     else:
         print('[-] No QR Code found in image')
         if owners:
@@ -47,6 +50,38 @@ def scan_image(img_path):
                 galleries[owner].append(img_path)
 
 
+def generate_qr_codes(guest_list):
+    # create folder named curret date hour minute second
+    date = str(datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S"))
+    folder_path = os.path.join(os.getcwd(), date)
+    os.mkdir(folder_path)
+    for guest in guest_list.keys():
+        # generate qr code
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=2,
+        )
+        qr.add_data(QR_CODE_URL.format(guest))
+        qr.make(fit=True)
+        img = qr.make_image()
+        img.save(os.path.join(folder_path, guest + '.png'))
 
-scan_folder('.')
-print(galleries)
+
+def generate_guest_list_from_csv(csv_file_path):
+    data = pd.read_csv(csv_file_path)
+    guests = {}
+    for index, row in data.iterrows():
+        guests[str(uuid.uuid4())] = {"first_name": row['first name'], "last_name": row['last name'], "email": row['email'], "phone": row['phone']}
+    return guests
+
+def generate_qr_codes_from_csv(csv_file_path):
+    guests = generate_guest_list_from_csv(csv_file_path)
+    generate_qr_codes(guests)
+
+# generate_qr_codes(generate_guest_list_from_csv('guest_list.csv'))
+generate_qr_codes_from_csv('guest_list.csv')
+
+# scan_folder('.')
+# print(galleries)    
