446287c5c093e2492c64345553e2cc2932422cc2
Ofir <ofir.duchovne@gmail.com>
2021-12-13 16:04:09 +0200

    creating galleries in DB and title on QR

--- image_proccessing/scan_folder.py
+++ image_proccessing/scan_folder.py
@@ -2,6 +2,7 @@
 # $ apt-get install libzbar0 libzbar-dev
 # $ pip install zbarlight  # you can also use setuptools directly
 from PIL import Image
+from PIL import ImageDraw, ImageFont
 import zbarlight
 import qrcode
 import os
@@ -9,6 +10,21 @@ from pathlib import Path
 import pandas as pd
 import uuid
 import datetime
+import pymongo
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+client = pymongo.MongoClient("mongodb+srv://{userName}:{password}@cluster0.qtek2.mongodb.net/yourPhotos?retryWrites=true&w=majority".format(userName=DB_USER, password=DB_PASSWORD))
+db = client.yourPhotos
+Events = db.events 
+Galleries = db.galleries
+
 
 NULL_CODE = 'NULL'
 QR_CODE_URL = "localhost:5000/gallery/{}"
@@ -19,10 +35,10 @@ galleries = {}
 def scan_folder(folder):
     # scan all images in folder by date and time of creation
     paths = sorted(Path(folder).iterdir(), key=os.path.getmtime)
-    
     # for img_name in os.listdir(folder):
     for img_name in paths:
-        img_path = os.path.join(folder, img_name)
+        # img_path = os.path.join(folder, img_name)
+        img_path = './' + str(img_name)
         if img_path.endswith('.png') or img_path.endswith('.jpg'):
             # scan image
             scan_image(img_path)
@@ -35,53 +51,74 @@ def scan_image(img_path):
     codes = zbarlight.scan_codes('qrcode', img)
     if codes:
         if codes[0].decode('utf-8') == NULL_CODE:
-            print('[#] Found QR Code: {}'.format(NULL_CODE))
             owners = None
+            print('[#] Found QR Code: {}'.format(NULL_CODE))
         else:
-            print('[+] Found QR Code: {}'.format(owners))
             owners = [x.decode('utf-8').split('/')[-1] for x in codes]
+            print('[+] Found QR Code: {}'.format(owners))
     else:
         print('[-] No QR Code found in image')
         if owners:
-
             for owner in owners:
                 if owner not in galleries:
                     galleries[owner] = []
                 galleries[owner].append(img_path)
+                Galleries.update_one({"id": owner}, {"$push": {"photos": img_path}})
 
 
-def generate_qr_codes(guest_list):
+def generate_qr_codes(guest_list,event_name):
     # create folder named curret date hour minute second
     date = str(datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S"))
     folder_path = os.path.join(os.getcwd(), date)
     os.mkdir(folder_path)
-    for guest in guest_list.keys():
+    for guest in guest_list:
         # generate qr code
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
-            border=2,
+            box_size=15,
+            border=4,
         )
-        qr.add_data(QR_CODE_URL.format(guest))
+        qr.add_data(QR_CODE_URL.format(guest["id"]))
         qr.make(fit=True)
         img = qr.make_image()
-        img.save(os.path.join(folder_path, guest + '.png'))
+        width, height = img.size
+        draw = ImageDraw.Draw(img)
+        # font = ImageFont.truetype('ariel.ttf', 24)
+        font = ImageFont.truetype("Ubuntu-B.ttf", 36)
+        draw.text((40, height - 47), guest["first_name"].capitalize() + " " + guest["last_name"].capitalize(), font=font)
+        draw.text((40, 0 + 5), event_name.title(), font=font)
+        img.save(os.path.join(folder_path, guest["id"] + '.png'))
 
 
-def generate_guest_list_from_csv(csv_file_path):
+def generate_guest_list_from_csv(csv_file_path,event_id):
     data = pd.read_csv(csv_file_path)
-    guests = {}
+    guests = []
     for index, row in data.iterrows():
-        guests[str(uuid.uuid4())] = {"first_name": row['first name'], "last_name": row['last name'], "email": row['email'], "phone": row['phone']}
+        # guests[str(uuid.uuid4())] = {"first_name": row['first name'], "last_name": row['last name'], "email": row['email'], "phone": row['phone']}
+        guests.append({"id": str(uuid.uuid4()), "first_name": row['first name'], "last_name": row['last name'], "email": row['email'], "phone": row['phone'],"photos":[], "event": event_id})
     return guests
 
-def generate_qr_codes_from_csv(csv_file_path):
-    guests = generate_guest_list_from_csv(csv_file_path)
-    generate_qr_codes(guests)
+def generate_qr_codes_from_csv(csv_file_path,event_name):
+    event_id = str(uuid.uuid4())
+    guests = generate_guest_list_from_csv(csv_file_path,event_id)
+    generate_qr_codes(guests,event_name)
+    guests_id = [guest["id"] for guest in guests]
+    print(guests)
+    Galleries.insert_many(guests)
+    newEvent = {
+        "event_id": event_id,
+        "event_name": event_name,
+        "event_date": datetime.datetime.now(),
+        "event_owner": "test",
+        "guests": guests_id
+    }
+    Events.insert_one(newEvent)
 
 # generate_qr_codes(generate_guest_list_from_csv('guest_list.csv'))
-generate_qr_codes_from_csv('guest_list.csv')
+# generate_qr_codes_from_csv('guest_list.csv', "our first event!")
 
-# scan_folder('.')
-# print(galleries)    
+# print(col.find_one({"guests": {"first_name":"koral"}}))
+
+scan_folder('./2021-12-13_16:01:48')
+# print(galleries)      
