c908ca2e159d8d8ea9c30b41ba2f9aa1258a5bfb
shoval zohar <shovalz1255@email.com>
2022-01-08 14:31:06 +0200

    finish info page

--- frontend/scripts/info.js
+++ frontend/scripts/info.js
@@ -1,3 +1,4 @@
+let user = {};
 $(document).ready(function () {
     let idUrl = getIdFromUrl($(location).attr("search"));
     getEvent(idUrl)
@@ -17,23 +18,78 @@ const getIdFromUrl = (_url) => {
 }
 
 
+
 const getEvent = (_id) => {
     $.ajax({
         type: "GET",
         url: "http://localhost:3000/api/gallery/" + _id,
         success: function (data) {
-           
-            let html = `
-                <strong>First Name:</strong> ${data[0].first_name} <br>
-                <strong>Last Name:</strong> ${data[0].last_name} <br>
-                <strong>Phone:</strong> ${data[0].phone} <br>
-                <strong>Email:</strong> ${data[0].email} <br>
+            user = data[0];
+            if(user.photos){
+                let html = `
+                <strong>First Name:</strong> ${user.first_name} <br>
+                <strong>Last Name:</strong> ${user.last_name} <br>
+                <strong>Phone:</strong> ${user.phone} <br>
+                <strong>Email:</strong> ${user.email} <br>
             `;
 
             $("#contactInfo").empty();
             $("#contactInfo").html(html);
-            $("#update_btn").prop("href","./form.html?id="+_id)
+            // $("#update_btn").prop("href","./form.html?id="+_id)
+            }
+            else{
+                window.location.href = "./gallery.html?id="+_id;
+            }
+        
 
         }
     })
+}
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("update_btn");
+let span = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+    modal.style.display = "block";
+    $("#first_name").val(user.first_name);
+    $("#last_name").val(user.last_name);
+    $("#phone").val(user.phone);
+    $("#email").val(user.email);
+    $("#update_contact").submit(function (e) {
+        let idUrl = getIdFromUrl($(location).attr("search"));
+        e.preventDefault();
+        let fn = $("#first_name").val();
+        let ln = $("#last_name").val();
+        let pn = $("#phone").val();
+        let em = $("#email").val();
+
+        $.ajax({
+            type: "PUT",
+            url: "http://localhost:3000/api/gallery/" + idUrl,
+            data: {
+                first_name: fn,
+                last_name: ln,
+                phone: pn,
+                email: em
+            },
+            success: function (data) {
+                location.reload();
+                modal.style.display = "none";
+            },
+            error: function (error) {
+                console.log(error);
+            }
+
+        })
+
+    })
+
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
